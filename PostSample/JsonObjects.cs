71330c576aa150d65ee8702807e386f1a4bcf1f4
David Zeller <david.zeller@outlook.com>
2019-02-15 13:46:04 +0100

    First version

---
+++ PostSample/JsonObjects.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostSample.JsonObjects.TestResults
+{
+    public class AssignedTo
+    {
+        public string displayName { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Configuration
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class LastTestRun
+    {
+        public string id { get; set; }
+    }
+
+    public class LastResult
+    {
+        public string id { get; set; }
+    }
+
+    public class TestCase
+    {
+        public string id { get; set; }
+    }
+
+    public class WorkItem
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+
+    public class WorkItemProperty
+    {
+        public WorkItem workItem { get; set; }
+    }
+
+    public class RunBy
+    {
+        public string displayName { get; set; }
+        public string id { get; set; }
+    }
+
+    public class LastResultDetails
+    {
+        public int duration { get; set; }
+        public DateTime dateCompleted { get; set; }
+        public RunBy runBy { get; set; }
+    }
+
+    public class TestResult
+    {
+        public int id { get; set; }
+        public string url { get; set; }
+        public AssignedTo assignedTo { get; set; }
+        public bool automated { get; set; }
+        public Configuration configuration { get; set; }
+        public LastTestRun lastTestRun { get; set; }
+        public LastResult lastResult { get; set; }
+        public string outcome { get; set; }
+        public string state { get; set; }
+        public string lastResultState { get; set; }
+        public TestCase testCase { get; set; }
+        public List<WorkItemProperty> workItemProperties { get; set; }
+        public LastResultDetails lastResultDetails { get; set; }
+        public string lastRunBuildNumber { get; set; }
+    }
+
+    public class TestResultList
+    {
+        public List<TestResult> value { get; set; }
+        public int count { get; set; }
+    }
+}
+
+namespace PostSample.JsonObjects.Projects
+{
+    public class Project
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string state { get; set; }
+        public int revision { get; set; }
+        public string visibility { get; set; }
+        public DateTime lastUpdateTime { get; set; }
+        public string description { get; set; }
+    }
+
+    public class ProjectList
+    {
+        public int count { get; set; }
+        public List<Project> value { get; set; }
+    }
+}
+
+namespace PostSample.JsonObjects.TestSuite
+{
+    public class Project
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string state { get; set; }
+        public string visibility { get; set; }
+        public DateTime lastUpdateTime { get; set; }
+    }
+
+    public class Avatar
+    {
+        public string href { get; set; }
+    }
+
+    public class Links
+    {
+        public Avatar avatar { get; set; }
+    }
+
+    public class LastUpdatedBy
+    {
+        public string displayName { get; set; }
+        public string url { get; set; }
+        public Links _links { get; set; }
+        public string id { get; set; }
+        public string uniqueName { get; set; }
+        public string imageUrl { get; set; }
+        public string descriptor { get; set; }
+    }
+
+    public class Plan
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Self
+    {
+        public string href { get; set; }
+    }
+
+    public class TestCases
+    {
+        public string href { get; set; }
+    }
+
+    public class TestPoints
+    {
+        public string href { get; set; }
+    }
+
+    public class Links2
+    {
+        public Self _self { get; set; }
+        public TestCases testCases { get; set; }
+        public TestPoints testPoints { get; set; }
+    }
+
+    public class DefaultConfiguration
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class TestSuite
+    {
+        public int id { get; set; }
+        public int revision { get; set; }
+        public Project project { get; set; }
+        public LastUpdatedBy lastUpdatedBy { get; set; }
+        public DateTime lastUpdatedDate { get; set; }
+        public Plan plan { get; set; }
+        public Links2 _links { get; set; }
+        public string suiteType { get; set; }
+        public string name { get; set; }
+        public bool inheritDefaultConfigurations { get; set; }
+        public List<DefaultConfiguration> defaultConfigurations { get; set; }
+    }
+
+    public class TestSuiteList
+    {
+        public List<TestSuite> value { get; set; }
+        public int count { get; set; }
+    }
+}
+
+namespace PostSample.JsonObjects.TestPlan
+{
+    public class Project
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string state { get; set; }
+        public string visibility { get; set; }
+        public DateTime lastUpdateTime { get; set; }
+    }
+
+    public class RootSuite
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Self
+    {
+        public string href { get; set; }
+    }
+
+    public class ClientUrl
+    {
+        public string href { get; set; }
+    }
+
+    public class RootSuite2
+    {
+        public string href { get; set; }
+    }
+
+    public class Links
+    {
+        public Self _self { get; set; }
+        public ClientUrl clientUrl { get; set; }
+        public RootSuite2 rootSuite { get; set; }
+    }
+
+    public class TestPlan
+    {
+        public int id { get; set; }
+        public Project project { get; set; }
+        public RootSuite rootSuite { get; set; }
+        public Links _links { get; set; }
+        public int revision { get; set; }
+        public string name { get; set; }
+        public string areaPath { get; set; }
+        public string iteration { get; set; }
+        public object owner { get; set; }
+        public string state { get; set; }
+    }
+
+    public class TestPlanList
+    {
+        public List<TestPlan> value { get; set; }
+        public int count { get; set; }
+    }
+}
--- PostSample/Program.cs
+++ PostSample/Program.cs
@@ -1,21 +1,85 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PostSample
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        public const string DevOpsTenant = "https://dev.azure.com/tenant/";
+        public const string User = "user@something.com";
+        public const string Personalaccesstoken = "enterpathere";
+        public const string OutputFile = @"C:\output.json";
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        static void Main(string[] args)
+        {  
+            var erg = new List<JsonObjects.TestResults.TestResult>();
+
+            foreach(var proj in GetProjects())
+            {
+                foreach (var plan in GetTestPlans(proj.name))
+                {
+                    foreach(var suite in GetTestSuites(proj.name, plan.id))
+                    {
+                        erg.AddRange(GetTestResultsAsJson(proj.name, plan.id, suite.id));
+                    }
+                }
+            }
+
+            System.IO.File.WriteAllText(OutputFile, JsonConvert.SerializeObject(erg));
+            System.Console.ReadLine();
+        }
+
+        public static List<JsonObjects.TestResults.TestResult> GetTestResultsAsJson(string projname, int planid, int suiteid)
+        {
+            string uri = DevOpsTenant + projname + "/_apis/test/Plans/" + planid + "/Suites/" + suiteid + "/points?api-version=5.0";
+            return GetDevOpsObjectFromUri<JsonObjects.TestResults.TestResultList>(uri).Result.value;
+        }
+
+        public static List<JsonObjects.Projects.Project> GetProjects()
+        {
+            string uri = DevOpsTenant + "/_apis/projects";
+            return GetDevOpsObjectFromUri<JsonObjects.Projects.ProjectList>(uri).Result.value;
+        }
+
+        public static List<JsonObjects.TestPlan.TestPlan> GetTestPlans(string projname)
+        {
+            string uri = DevOpsTenant + projname + "/_apis/testplan/plans?api-version=5.0-preview.1";
+            return GetDevOpsObjectFromUri<JsonObjects.TestPlan.TestPlanList>(uri).Result.value;
+        }
+
+        public static List<JsonObjects.TestSuite.TestSuite> GetTestSuites(string projname, int testplanid)
+        {
+            string uri = DevOpsTenant + projname + "/_apis/testplan/Plans/" + testplanid.ToString() + "/suites?api-version=5.0-preview.1";
+            return GetDevOpsObjectFromUri<JsonObjects.TestSuite.TestSuiteList>(uri).Result.value;
+        }
+
+        public static async Task<T> GetDevOpsObjectFromUri<T>(string uri) 
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", User, Personalaccesstoken))));
+                    using (HttpResponseMessage response = await client.GetAsync(uri))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        return JsonConvert.DeserializeObject<T>(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return default(T);
         }
     }
 }
